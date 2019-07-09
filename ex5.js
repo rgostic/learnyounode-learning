a581c8febff7ce3013baabe48151af353b8f6ae2
rgostic <5856138+rgostic@users.noreply.github.com>
2019-07-09 17:04:39 -0400

    Problem 5

--- ex5.js
+++ ex5.js
@@ -1,1 +1,24 @@
-ex5.js
+var fs = require('fs');
+
+var dirpath = process.argv[2];
+var ext = process.argv[3];
+
+fs.readdir(dirpath, function(err, data) {
+  var foundFiles = data.filter(function (filepath) {
+
+    if (filepath.indexOf('.') === -1) {
+      return false;
+    }
+
+    var splitPath = filepath.split('.');
+    var fileExt = splitPath[splitPath.length - 1];
+    if (fileExt === ext) {
+      return true;
+    }
+    return false;
+  });
+
+  foundFiles.forEach(function (filepath) {
+    console.log(filepath);
+  })
+});
